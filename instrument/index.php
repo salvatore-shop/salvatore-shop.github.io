8f2068eabded0ee5ea8c8839a2da80129192a8c7
salvatore-shop <salvatore.miyoshi@gmail.com>
2017-05-24 18:05:44 +0900

    Add files via upload

---
+++ instrument/index.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>楽器リスト | SALVATORE</title>
+
+    <link href="../css/bootstrap.css" rel="stylesheet">
+    <link href="../css/style.css" rel="stylesheet">
+    <link href="css/instrument_style.css" rel="stylesheet">
+
+
+  </head>
+  <body>
+
+   <div class="container-fluid">
+	<div class="row">
+		<div class="col-md-12">
+        
+		<?php include_once($_SERVER["DOCUMENT_ROOT"].'/include/header.php'); ?>
+
+		<main>
+        
+        <h2>楽器リスト</h2>
+        
+        <!-- instrument_1 -->
+        <a href="vn/0006_oreste.php">
+        <div class="inst_box">
+            <div class="inst_text">
+                <h3>Violin<br>Martini ORESTE 1927 Mantova</h3>
+                <p class="mt20">ボディサイズ356mmの、平均的サイズ。<br>
+                １枚板の裏板やネック材が、とても美しい楽器です。<br>
+                最近まで演奏家が弾いていた楽器で、力強く濃密な音が魅力です。</p>
+                <p class="price">価格 3,240,000円</p>
+             </div><!-- //inst_text -->
+             <div class="inst_img"><img src="vn/img/oreste1.jpg"/></div>
+        </div><!-- //inst_box -->
+        </a>
+        
+        <!-- instrument_2 -->
+<!--        <a href="vn/0002_endo.php">
+        <div class="inst_box">
+            <div class="inst_text">
+                <h3>中古 Violin<br>YUKIYO ENDO 3/4 アマティモデル</h3>
+                <p class="mt20">分数とは思えない響き！！弓・ケース付き<br>
+                楽器：YUKIYO ENDO 3/4 アマティモデル 2009 Tokyo<br>
+                弓：FINKEL 160 F.C.Neuveville<br>
+                ケース：GEWA ひょうたん 茶色<br>
+                日本の製作家、遠藤氏の作品です。<br>
+                フルサイズにも負けない音量を実現している超お薦めヴァイオリンです。<br>
+                音色も上品で、低音から高音まで非常にバランスよく健康的です。<br>
+                弓は、FINKEL工房製最上級クラスのFINKEL 160 F.C.Neuveville。
+                スティックの腰も強く、操作性も抜群です。</p>
+                <p class="price">価格 450,000円<br>(楽器本体のみ 400,000円)</p>
+             </div>
+             <div class="inst_img"><img src="vn/img/endo1.jpg"/></div>
+        </div>
+        </a>
+-->        
+
+        <!-- instrument_3 -->
+        <a href="vn/0005_giordano_set.php">
+        <div class="inst_box">
+            <div class="inst_text">
+                <h3>Violin【特価１台限り】<br>1/4サイズ　Carlo GIORDANO VL-1</h3>
+                <p class="mt20">楽器・弓・ケース（黒）・松脂・肩当、ヴァイオリン演奏に必要なものが全てそろった入門者向けセットです。<br>
+身長115ｃｍ～125ｃｍのお子様に適応したサイズです。</p>
+                <p class="price"><span class="line-through">参考価格 43,200円</span><br>売値 32,400円</p>
+             </div><!-- //inst_text -->
+             <div class="inst_img"><img src="vn/img/giordano_set.jpg"/></div>
+        </div><!-- //inst_box -->
+        </a>
+        
+        <!-- instrument_4 -->
+        <a href="vn/0004_oldgerman.php">
+        <div class="inst_box">
+            <div class="inst_text">
+                <h3>Violin<br>Old German Violin</h3>
+                <p class="mt20">古いヴァイオリンのイメージそのものといえる風合の楽器です。<br>
+                音はマイルドで暖か味があり、クラシック以外のジャンル、ジプシー・ブルーグラス・ジャスでも使えそう。<br>
+                キンキンと金属的な音が気になる人にもお奨め！<br>
+                ペグ・駒・魂柱を新品に交換し、ストレス無く演奏できます。</p>
+                <p class="price">価格 324,000円</p>
+             </div><!-- //inst_text -->
+             <div class="inst_img"><img src="vn/img/oldgerman1.jpg"/></div>
+        </div><!-- //inst_box -->
+        </a>
+        
+        <!-- instrument_5 -->
+        <a href="vn/0003_okubo.php">
+        <div class="inst_box">
+            <div class="inst_text">
+                <h3>Violin<br>Sam Okubo Tokorozawa 2015</h3>
+                <p>A.Stradivarius 1704 “Betts” モデル</p>
+                <p class="mt20">大久保氏の50作目のヴァイオリンです。<br>
+                節目の作品ということで、特に厳選された材が使われ、丁寧に作られています。<br>
+                木目のそろった裏板もすばらしく、大変美しい楽器です。</p>
+                <p class="soldout">sold out</p>
+             </div><!-- //inst_text -->
+             <div class="inst_img"><img src="vn/img/okubo1.jpg"/></div>
+        </div><!-- //inst_box -->
+        </a>       
+        
+                <!-- instrument_6 -->
+        <a href="vn/0001_schade.php">
+        <div class="inst_box">
+            <div class="inst_text">
+                <h3>Joachim SCHADE Halle</h3>
+                <p>Guarneri del Gesù “Vieuxtemps”model 証明書付</p>
+                <p class="mt20">Joachim Schade の楽器は、即戦力として多くのプロ奏者に愛用され、またアンティークフィニッシュの腕前は世界最高峰です。<br>
+                サイズは標準の356mmで、パワーのある豊かな音量が魅力です。<br>
+                ネックは細めで、女性や手の小さな人にもお薦めできます。</p>
+                <p class="soldout">sold out</p>
+             </div><!-- //inst_text -->
+             <div class="inst_img"><img src="vn/img/schade1.jpg"/></div>
+        </div><!-- //inst_box -->
+        </a>
+        
+        
+      
+            
+    	</main>
+            
+             <!--フッターを追加-->
+		    <footer class="container-fluid">
+      		<small>Copyright © 2008-2016 SALVATORE Co.,Ltd. All Rights Reserved.</small>
+    		</footer>
+        
+		</div>
+	</div>
+</div>
+
+    <script src="../js/jquery.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+    <script src="../js/scripts.js"></script>
+  </body>
+</html>
