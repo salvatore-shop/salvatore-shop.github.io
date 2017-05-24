155cf15fa2b1e9a288ab234996bef054d783370f
salvatore-shop <salvatore.miyoshi@gmail.com>
2017-05-24 18:01:55 +0900

    add index.php
    Replaced header with include

---
+++ index.php
@@ -0,0 +1,256 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>SALVATORE 池袋の弦楽器専門店 サルヴァトーレ</title>
+
+	<meta name="keywords" content="サルヴァトーレ,サルバトーレ,salvatore,池袋,バイオリン,ヴァイオリン,弦楽器">
+    <meta name="description" content="サルヴァトーレは池袋にある弦楽器専門店です">
+
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    
+  </head>
+  
+  <body>    
+    <div class="container-fluid">
+	<div class="row">
+		<div class="col-md-12">
+        
+		<?php include_once($_SERVER["DOCUMENT_ROOT"].'/include/header.php'); ?>
+
+	<main>
+			<p>弦楽器(ヴァイオリン、ヴィオラ、チェロ)、ケース、弦・小物の販売と修理のお店です。<br />
+			弦楽器業界を20年経験後、10年前より池袋でSALVATOREを営業しています。お気軽にお立ち寄りください。</p>
+
+      <!--カルーセル-->
+			<div class="carousel slide" id="carousel-852529"  data-ride="carousel" data-interval="9000">
+				<ol class="carousel-indicators">
+					<li data-slide-to="0" data-target="#carousel-852529" class="active">
+					</li>
+					<li data-slide-to="1" data-target="#carousel-852529">
+					</li>
+					<li data-slide-to="2" data-target="#carousel-852529">
+					</li>
+				</ol>
+                <!-- ここからCarouselの中身 -->
+				<div class="carousel-inner">
+					<div class="item active">
+						<img alt="Carousel First" src="img/shop_img_1.jpg">
+						<div class="carousel-caption">
+							<h4>
+								弓の「毛替え」承ります
+							</h4>
+							<p>
+								馬毛の品質によってエクセレント、スタンダードの２種類がございます。<br>
+								ご希望に応じてお選びください。
+							</p>
+						</div>
+					</div>
+					<div class="item">
+						<img alt="Carousel Second" src="img/shop_img_2.jpg">
+						<div class="carousel-caption">
+							<h4>
+								楽器の選定
+							</h4>
+							<p>
+								新作、オールド、名器...<br>
+								ご要望に合わせた楽器をお探しいたします。
+							</p>							
+						</div>
+					</div>
+					<div class="item">
+						<img alt="Carousel Third" src="img/shop_img_3.jpg">
+						<div class="carousel-caption">
+							<h4>
+								営業時間 11:00 - 19:00
+							</h4>
+							<p>
+								毎日営業しております。<br>
+								休業日は当サイトでご案内いたします。
+							</p>
+						</div>
+					</div>
+				</div> <a class="left carousel-control" href="#carousel-852529" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a> <a class="right carousel-control" href="#carousel-852529" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+			</div>
+            　<!-- ここからPickUp -->
+    
+			<div class="row items-container">
+            
+				<div class="col-md-4 item"><!--その1-->
+					<div class="thumbnail">
+						<img alt="馬毛" src="img/bowhair.jpg">
+						<div class="caption">
+							<h3>毛替え 承ります</h3>
+							<p>店内で毛替えしています。<br>
+							当店では、２種類の毛替えコースを用意しております。</p>
+
+							<table class="table_1">
+							    <tbody>
+							    <tr>
+							      <th scope="col">&nbsp;</th>
+							      <th scope="col">スタンダード<br>★</th>
+							      <th scope="col">エクセレント<br>★★★</th>
+							    </tr>
+							    <tr>
+							      <th scope="row">Violin</th>
+							      <td>5,000円</td>
+							      <td>8,000円</td>
+							    </tr>
+							    <tr>
+							      <th scope="row">Violin分数</th>
+							      <td>4,320円</td>
+							      <td>-</td>
+							    </tr>
+							    <tr>
+							      <th scope="row">Viola</th>
+							      <td>5,500円</td>
+							      <td>8,500円</td>
+							    </tr>
+							    <tr>
+							      <th scope="row">Cello</th>
+							      <td>6,000円</td>
+							      <td>9,000円</td>
+							    </tr>
+							    <tr>
+							      <th scope="row">Bass</th>
+							      <td>8,000円</td>
+							      <td>11,000円</td>
+							    </tr>
+							  </tbody>
+							</table>
+							<p>
+								Highest Selection-TopQuality<br>
+								最上質モンゴル馬毛を使用しています。
+							</p>
+						  	<p class="pickup_btn">
+								<a class="btn btn-primary" href="mailto:salvatore@aria.ocn.ne.jp">お問合せはこちら</a>
+							</p>
+						</div>
+					</div>
+				</div>
+                
+				<div class="col-md-4 item"><!--old-->
+					<div class="thumbnail">
+						<img src="instrument/vn/img/oldgerman1.jpg" width="200px">
+						<div class="caption">
+		                    <h3>Violin<br>
+		                    Old German Violin</h3>
+							<a href="instrument/vn/0004_oldgerman.php"></a>
+							<p>古いヴァイオリンのイメージそのものといえる風合の楽器です。<br>
+							音はマイルドで暖か味があり、クラシック以外のジャンル、ジプシー・ブルーグラス・ジャスでも使えそう。<br>
+							キンキンと金属的な音が気になる人にもお奨め！</p>
+							<p>ペグ・駒・魂柱を新品に交換し、ストレス無く演奏できます。</p>						
+							<p class="price">価格 324,000円</p>
+						</div>
+					</div>
+				</div>
+                                
+
+				<div class="col-md-4 item">
+					<div class="thumbnail">
+                    	<iframe width="640" src="https://www.youtube.com/embed/ZSARIZDFWAI?rel=0" frameborder="0" allowfullscreen></iframe>
+						<div class="caption">
+							<p>Old German Violinを使用した演奏です。<br>
+							カントリーやブルーグラスのような音楽にも最適です。</p>
+                            <p class="pb20 bb_dot">演奏曲「Sally Ann」</p>
+						</div><!-- //caption -->
+                        
+          				<iframe width="640" src="https://www.youtube.com/embed/qLOmwm3ZM8k?rel=0" frameborder="0" allowfullscreen></iframe>
+						<div class="caption">
+							<p>Old German Violinを使用した演奏です。<br>
+							ヴァイオリンの定番曲。オールドならではの柔らかな音色です。</p>
+                            <p>演奏曲「タイスの瞑想曲」</p>                       			 
+						</div><!-- //caption -->
+					</div><!-- //thumbnail -->
+				</div>
+			</div><!-- end row -->
+				
+
+			<div class="row items-container">
+				<div class="col-md-4 item"><!--1/4giordano_set-->
+					<div class="thumbnail">
+						<img src="instrument/vn/img/giordano_set.jpg">
+						<div class="caption">
+		                    <h3>Violin<br>
+		                    【特価１台限り】<br>
+							1/4サイズ　Carlo GIORDANO VL-1</h3>
+							<a href="instrument/vn/0005_giordano_set.php"></a>
+							<p>楽器・弓・ケース（黒）・松脂・肩当、ヴァイオリン演奏に必要なものが全てそろった入門者向けセットです。<br>
+							身長115ｃｍ～125ｃｍのお子様に適応したサイズです。</p>						 
+							<p class="price"><s>参考価格 43,200円</s><br>		 
+							売値 32,400円</p>
+						</div>
+					</div>
+				</div>
+                
+				<div class="col-md-4 item"><!--inst_1-->
+					<div class="thumbnail">
+						<img alt="schade" src="instrument/vn/img/oreste1.jpg" width="200px">
+						<div class="caption">
+                        	<h3>Violin</h3>
+							<h3>Martini ORESTE 1927 Mantova</h3>
+							<a href="instrument/vn/0006_oreste.php"></a>
+							<p>ボディサイズ356mmの、平均的サイズ。<br>
+                            １枚板の裏板やネック材が、とても美しい楽器です。<br>
+                            最近まで演奏家が弾いていた楽器で、力強く濃密な音が魅力です。</p>
+                            <p class="price">価格 3,240,000円</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-md-4 item"><!--Okubo-->
+					<div class="thumbnail">
+						<img src="instrument/vn/img/okubo1.jpg" width="200px">
+						<div class="caption">
+		                    <h3>Violin<br>
+		                    Sam Okubo Tokorozawa 2015<br>
+							A.Stradivarius 1704 “Betts” モデル</h3>
+							<a href="instrument/vn/0003_okubo.php"></a>
+							<p>大久保氏の50作目のヴァイオリンです。</p>
+							<p>節目の作品ということで、特に厳選された材が使われ、丁寧に作られています。</p>
+							<p>木目のそろった裏板もすばらしく、大変美しい楽器です。</p>
+							<p class="soldout">sold out</p>
+						</div>
+					</div>
+				</div>
+			</div><!-- end row -->
+            
+        </div><!-- // col-md-12-->
+        </div><!-- // row-->
+    </div><!-- // container-fluid-->
+
+	</main>
+    
+    <!--フッターを追加-->
+	<footer class="container-fluid">
+    <small>Copyright © 2008-2016 SALVATORE Co.,Ltd. All Rights Reserved.</small>
+	</footer>
+    
+	
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/scripts.js"></script>
+
+	<!--<script>
+	$(function(){
+		  $('.item').matchHeight();
+	});
+	</script>-->
+    
+    <script><!--google analytics-->
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-85892160-1', 'auto');
+  ga('send', 'pageview');
+	</script>
+
+  </body>
+</html>
